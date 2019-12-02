eac055e3e939cce4588c8b946304b70ce0020eea
Luis Ribeiro <luissenac@gmail.com>
2019-12-02 16:43:44 -0300

    Primeira linha: em casa - PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Primeira linha: em casa - PHP";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <h1>Exemplo de site no GitHub</h1>
+        <p>Primeira linha: em casa</p>
+    </body>
+</html>
